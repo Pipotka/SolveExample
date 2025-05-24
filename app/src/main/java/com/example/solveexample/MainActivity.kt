8746169b40e0d8479bffc3105c3645fee7fa03f8
Pipotka <157719919+Pipotka@users.noreply.github.com>
2025-05-24 11:48:35 +0300

    1.0

--- app/src/main/java/com/example/solveexample/MainActivity.kt
+++ app/src/main/java/com/example/solveexample/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.solveexample.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,31 +20,27 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 
     fun onClickStart(control : View){
         runNewProblem()
         binding.start.isEnabled = false
-        binding.checkAnswer.isEnabled = true
+        binding.correctButton.isEnabled = true
+        binding.incorrectButton.isEnabled = true
     }
 
-    fun onClickCheck(control : View){
-        binding.answer.isEnabled = false
-        binding.checkAnswer.isEnabled = false
-        var answer = 0
-        try {
-            answer = binding.answer.text.toString().toInt()
+    fun onClickButton(control : View){
+        var userChoice = when (control.id){
+            binding.correctButton.id -> true
+            binding.incorrectButton.id -> false
+            else -> false
         }
-        catch (ex: Exception)
-        {
-            binding.answer.isEnabled = true
-            binding.checkAnswer.isEnabled = true
-            animateBackground(ContextCompat.getColor(this, R.color.red),
-                ContextCompat.getColor(this, R.color.white),
-                600)
-            return
-        }
+
+
+        binding.correctButton.isEnabled = false
+        binding.incorrectButton.isEnabled = false
+
+        var answer = binding.answer.text.toString().toInt()
 
         if (answer == problemBook.getAnswer(problem)){
             problemBook.correctlySolvedProblems++
@@ -59,8 +56,8 @@ class MainActivity : AppCompatActivity() {
 
         showState()
         runNewProblem()
-        binding.answer.isEnabled = true
-        binding.checkAnswer.isEnabled = true
+        binding.correctButton.isEnabled = true
+        binding.incorrectButton.isEnabled = true
     }
 
     private fun animateBackground(startColor: Int,
@@ -79,12 +76,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun generateAnswer(): Int{
+        if (Random.nextInt(0, 2) == 1){
+            return problemBook.getAnswer(problem)
+        }
+        return Random.nextInt(10, 100)
+    }
+
     private fun runNewProblem(){
         problem = problemBook.generateProblem()
         binding.firstNumber.text = problem.firstOperand.toString()
         binding.operation.text = problem.operator.toString()
         binding.secondNumber.text = problem.secondOperand.toString()
-        binding.answer.setText("")
+        binding.answer.text = generateAnswer().toString()
     }
 
     private fun showState(){
@@ -92,9 +96,5 @@ class MainActivity : AppCompatActivity() {
         binding.correctCount.text = problemBook.correctlySolvedProblems.toString()
         binding.incorrectCount.text = problemBook.incorrectlySolvedProblems.toString()
         binding.percentCorrect.text = "${"%.2f".format(problemBook.getPercentageOfCorrectAnswers())}%"
-    }
-
-    private fun clearBackground(){
-        binding.root.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
     }
 }
